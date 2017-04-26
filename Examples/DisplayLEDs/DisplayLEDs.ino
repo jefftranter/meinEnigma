327514962380219b0beb93e88162ce028f4764d7
Jeff Tranter <tranter@pobox.com>
2017-04-26 16:40:10 -0400

    Code clean up.

--- Examples/DisplayLEDs/DisplayLEDs.ino
+++ Examples/DisplayLEDs/DisplayLEDs.ino
@@ -3,6 +3,8 @@
 
   Demonstrates controlling the 4 alphanumeric LEDs on the main board.
 
+  Uses code from the MeinEnigma software.
+
   Jeff Tranter <tranter@pobox.com.
 
 */
@@ -15,22 +17,21 @@
 
 HT16K33 HT;
 
-// Display something on one of the "wheels".
-// Rightmost wheel is 3
-// Leftmost is 0.
-void displayLetter(char letter, uint8_t walzeno) {
+// Display a character something on one of the displays.
+// Leftmost display is 0, rightmost 3.
+void displayLetter(char letter, uint8_t dispeno) {
   uint8_t led;
   int8_t i;
   uint16_t val;
 
-  led = (walzeno) * 16;
-  if (letter>'`')
-    letter-=('a'-'A');
-  val = pgm_read_word(fontTable+letter-' '); // A= 0b1111001111000000 - 0xF3C0
+  led = (dispeno) * 16;
+  if (letter > '`')
+    letter -= ('a' - 'A');
+  val = pgm_read_word(fontTable + letter - ' ');
 
   // No lookup table needed, all LEDs are at offset 64
-  HT.setDisplayRaw(walzeno*2+64/8,val & 0xFF);
-  HT.setDisplayRaw(walzeno*2+64/8+1,val>>8);
+  HT.setDisplayRaw(dispeno * 2 + 64 / 8, val & 0xFF);
+  HT.setDisplayRaw(dispeno *2 + 64 / 8 + 1, val>>8);
   HT.sendLed();
 }
 
@@ -39,7 +40,7 @@ void setup() {
   // work. This also clears all display segments and LEDs.
   HT.begin(0x00);
 
-  // Clear the ram to make sure we read data but don't send it!
+  // Clear the RAM to make sure we read data but don't send it.
   for (int i = 0; i < sizeof(HT.displayRam); i++)
     HT.displayRam[i] = 0;
 }
@@ -47,24 +48,26 @@ void setup() {
 void loop() {
   int i;
 
-  // All on
+  // All segments on
   for (i = 64; i <= 127; i++) {
     HT.setLedNow(i);
     delay(50);
   }
 
-  // All off
+  // All segments off
   for (i = 64; i <= 127; i++) {
     HT.clearLedNow(i);
     delay(50);
   }
 
+  // Display some characters
   displayLetter('A', 0);
   displayLetter('B', 1);
   displayLetter('C', 2);
   displayLetter('D', 3);
   delay(1000);
 
+  // Display full character set.
   int j = 0;
   for (char c = ' '; c <= '`'; c++) {
     displayLetter(c, j % 4);
